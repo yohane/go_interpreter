a282c9d273c972606999d800c6d20d3b12e40117
nemoto <nemoto@nagisa-inc.jp>
2018-10-01 09:07:58 +0900

    ページ206　firstの実装まで

--- 04/src/monkey/evaluator/evaluator_test.go
+++ 04/src/monkey/evaluator/evaluator_test.go
@@ -348,6 +348,12 @@ func TestBuiltinFunctions(t *testing.T) {
 		{`len("hello world")`, 11},
 		{`len(1)`, "argument to `len` not supported, got INTEGER"},
 		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
+		{`len([1,2,3])`, 3},
+		{`len([])`, 0},
+		{`puts("hello","world!")`, nil},
+		{`first([1,2,3])`, 1},
+		{`first([])`, nil},
+		{`first(1)`, "argument to `first` must be ARRAY, got INTEGER"},
 	}
 
 	for _, tt := range tests {
